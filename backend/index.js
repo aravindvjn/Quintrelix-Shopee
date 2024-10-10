f1867b4ce0720ed24a73c9ca9a7ec9682f4b4882
aravind <aravind284479@gmail.com>
2024-10-10 21:25:33 +0530

    hosting

--- backend/index.js
+++ backend/index.js
@@ -30,13 +30,23 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //DataBase Connection
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
+
+pool
+  .connect()
+  .then(() => console.log("Connected to Neon PostgreSQL database!"))
+  .catch((err) => console.error("Connection error", err.stack));
 
 //AUTH
 //register
