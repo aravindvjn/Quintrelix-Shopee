9b78705ed1ac6151e85efd9cde6c5dda2fd3caa0
aravind <aravind284479@gmail.com>
2024-10-10 22:40:01 +0530

    deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ import OrderReq from "./Pages/Admin/OrderReq/OrderReq";
 import Payment from "./Pages/Payment/Payment";
 import MoreInformation from "./Pages/MoreInformation/MoreInformation";
 import TrackInfo from "./Pages/TrackInfo/TrackInfo";
-import { UserProvider } from "./Pages/Context/context.jsx";
+import { UserProvider } from "./Pages/Context/context";
 
 function App() {
   return (
