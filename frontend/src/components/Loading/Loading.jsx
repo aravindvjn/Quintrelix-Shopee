345924f243feb319511c829c27ebb24839a7d583
aravind <aravind284479@gmail.com>
2024-10-10 23:11:07 +0530

    deploy

--- frontend/src/components/Loading/Loading.jsx
+++ frontend/src/components/Loading/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Loading.css'
 const Loading = () => {
   return (
-    <div className='center loading'>
+    <div className='loading'>
         <div className="spinner"></div>
     </div>
   )
---
+++ frontend/src/server.jsx
@@ -0,0 +1,4 @@
+
+const URL=import.meta.env.VITE_URL
+export const authURL=import.meta.env.VITE_AUTH_URL
+export default URL;
