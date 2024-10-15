7602fc5762de6c0910715062690c8a8ec7baaf6a
aravind <aravind284479@gmail.com>
2024-10-15 12:16:58 +0530

    middleware added

--- backend/index.js
+++ backend/index.js
@@ -52,6 +52,16 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin); // Dynamically set the origin
+  res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(
   session({
