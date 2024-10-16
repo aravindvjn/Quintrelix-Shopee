f93d2435922b0f6a27150ac0c7e73d0e71890bc5
aravind <aravind284479@gmail.com>
2024-10-16 10:28:34 +0530

    Undermaintenance

--- frontend/src/components/UnderMaintenance/UnderMaintenance.jsx
+++ frontend/src/components/UnderMaintenance/UnderMaintenance.jsx
@@ -3,14 +3,14 @@ import "./UnderMaintenance.css";
 const UnderMaintenance = () => {
   return (
     <div className="undermaintenance">
-      <div class="container">
+      <div className="container">
         <h1>We'll Be Back Soon</h1>
         <p>
           Our website is currently undergoing scheduled maintenance. We
           appreciate your patience and will be back shortly.
         </p>
-        <div class="loader"></div>
-        <div class="footer">Thank you for your understanding.</div>
+        <div className="loader"></div>
+        <div className="footer">Thank you for your understanding.</div>
       </div>
     </div>
   );
